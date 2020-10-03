cd73f12db9005d633ce5ad762031a2d0a063627d
JPfstack <juanpedro.jimenez@gmail.com>
2020-10-03 13:44:32 +0200

    Diseño de componentes user y login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent }
-
+  { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
